edb6b104b56f4c38fb9adf56d045372d43dd33f6
twtcom001 <twtcom001@sina.com>
2017-10-05 18:02:50 +0800

    20171005 1802
    Signed-off-by: twtcom001 <twtcom001@sina.com>

---
+++ app/models.py
@@ -0,0 +1,54 @@
+#coding: utf-8
+import hashlib
+from datetime import datetime
+from werkzeug.security import generate_password_hash, check_password_hash
+from flask.ext.login import UserMixin
+from . import db, login_manager
+
+
+class User(UserMixin, db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    email = db.Column(db.String(64), unique=True, index=True)
+    username = db.Column(db.String(64), unique=True, index=True)
+    password_hash = db.Column(db.String(128))
+    avatar_hash = db.Column(db.String(32))
+
+    @staticmethod
+    def insert_admin(email, username, password):
+        user = User(email=email, username=username, password=password)
+        db.session.add(user)
+        db.session.commit()
+
+    @property
+    def password(self):
+        raise AttributeError('password is not a readable attribute')
+
+    @password.setter
+    def password(self, password):
+        self.password_hash = generate_password_hash(password)
+
+    def verify_password(self, password):
+        return check_password_hash(self.password_hash, password)
+
+    def __init__(self, **kwargs):
+        super(User, self).__init__(**kwargs)
+        if self.email is not None and self.avatar_hash is None:
+            self.avatar_hash = hashlib.md5(
+                    self.email.encode('utf-8')).hexdigest()
+
+    def gravatar(self, size=40, default='identicon', rating='g'):
+        # if request.is_secure:
+        #     url = 'https://secure.gravatar.com/avatar'
+        # else:
+        #     url = 'http://www.gravatar.com/avatar'
+        url = 'http://gravatar.duoshuo.com/avatar'
+        hash = self.avatar_hash or hashlib.md5(
+            self.email.encode('utf-8')).hexdigest()
+        return '{url}/{hash}?s={size}&d={default}&r={rating}'.format(
+            url=url, hash=hash, size=size, default=default, rating=rating)
+
+
+# callback function for flask-login extentsion
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
--- manage.py
+++ manage.py
@@ -11,11 +11,6 @@ manager = Manager(app)
 migrate = Migrate(app, db)
 manager.add_command('db', MigrateCommand)
 
-
-def make_shell_context():
-    return dict(db=db)
-manager.add_command("shell", Shell(make_context=make_shell_context))
-
-
 if __name__ == '__main__':
-    manager.run()
+
+	app.run(debug=True, use_reloader=False)
